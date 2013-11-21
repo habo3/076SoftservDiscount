69bdf06ad6c206eb22e6dd36051ab155e2bb2dc7
Maxim <mr.dekio@yandex.ua>
2013-11-21 17:42:29 +0200

    Removing hardcode from PhoneFormatter

--- PhoneFormatter.m
+++ PhoneFormatter.m
@@ -17,9 +17,9 @@
 + (NSString *)numberForCall:(NSString *)phoneNumber withCity:(NSString *)city
 {
     NSString *clearNumber = [self removeAllCharactersFromNumber:phoneNumber];
-    NSString *cityNumber = [self cityNumberFromHardcodeVersionOfCityList:city];
+    NSString *cityNumber = [self cityNumberFormat:city];
     if(cityNumber == nil || [clearNumber hasPrefix:@"+380"] || [clearNumber hasPrefix:cityNumber])
-            return clearNumber;
+        return clearNumber;
     else
     {
         if(clearNumber.length == 10)
@@ -27,7 +27,7 @@
         NSString *newNumber = [cityNumber stringByAppendingString:clearNumber];
         return newNumber;
     }
-
+    
 }
 
 + (NSString *)numberForView:(NSString *)phoneNumber withCity:(NSString *)city
@@ -35,18 +35,18 @@
     NSString *callString = [self numberForCall:phoneNumber withCity:city];
     switch (callString.length) {
         case 10:
-             return [NSString stringWithFormat:@"(%@) %@-%@-%@",
-                     [callString substringToIndex:3],
-                     [callString substringWithRange:NSMakeRange(3, 3)],
-                     [callString substringWithRange:NSMakeRange(6, 2)],
-                     [callString substringWithRange:NSMakeRange(8, 2)]];
+            return [NSString stringWithFormat:@"(%@) %@-%@-%@",
+                    [callString substringToIndex:3],
+                    [callString substringWithRange:NSMakeRange(3, 3)],
+                    [callString substringWithRange:NSMakeRange(6, 2)],
+                    [callString substringWithRange:NSMakeRange(8, 2)]];
         case 13:
             return [NSString stringWithFormat:@"%@ (%@) %@-%@-%@",
                     [callString substringToIndex:3],
                     [callString substringWithRange:NSMakeRange(3, 3)],
                     [callString substringWithRange:NSMakeRange(6, 3)],
                     [callString substringWithRange:NSMakeRange(9, 2)],
-                    [callString substringWithRange:NSMakeRange(11, 2)]];            
+                    [callString substringWithRange:NSMakeRange(11, 2)]];
         default:
             return callString;
     }
@@ -73,37 +73,11 @@
     
     return  strippedString;
 }
-+(NSString *)cityNumberFromHardcodeVersionOfCityList:(NSString *)cityName
++(NSString *)cityNumberFormat:(NSString *)cityName
 {
-    if([cityName isEqualToString:@"Івано-Франківськ"])
-        return @"034";
-    if([cityName isEqualToString:@"Дніпропетровськ"])
-        return @"056";
-    if([cityName isEqualToString:@"Донецьк"])
-        return @"062";
-    if([cityName isEqualToString:@"Київ"])
-        return @"044";
-    if([cityName isEqualToString:@"Кривий Ріг"])
-        return @"056";
-    if([cityName isEqualToString:@"Львів"])
-        return @"032";
-    if([cityName isEqualToString:@"Мукачеве"])
-        return @"031";
-    if([cityName isEqualToString:@"Одеса"])
-        return @"048";
-    if([cityName isEqualToString:@"Полтава"])
-        return @"053";
-    if([cityName isEqualToString:@"Рівне"])
-        return @"036";
-    if([cityName isEqualToString:@"Севастополь"])
-        return @"069";
-    if([cityName isEqualToString:@"Ужгород"])
-        return @"031";
-    if([cityName isEqualToString:@"Харків"])
-        return @"057";
-    if([cityName isEqualToString:@"Чернівці"])
-        return @"037";
-    return nil;
+    NSDictionary *dictRoot = [NSDictionary dictionaryWithContentsOfFile:[[NSBundle mainBundle] pathForResource:@"Cities Formats" ofType:@"plist"]];
+    NSString *cityFormat = [NSString stringWithString:[dictRoot objectForKey:cityName]];
+    return cityFormat;
 }
 
 @end
